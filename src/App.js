88ef718a727704270433d0bd2932494239b99b84
Ricardo Andrade <ricardo.maia@ccc.ufcg.edu.br>
2021-07-25 22:04:06 -0300

    add product section, code to store in localstorage and product counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './images/logo.png';
+import productData from './productData';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    setData(productData);
+    let cartContent = JSON.parse(localStorage.getItem("cart"));
+    if (cartContent !== null) {
+      setCart(cartContent);
+    } 
+  },[]);
+
+  function handleCart(index) {
+      let newCart = cart;
+      newCart.push(data[index]);
+      setCart(newCart);
+      localStorage.setItem("cart",JSON.stringify(cart));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,10 +31,22 @@ function App() {
                   <li className="menu-item"><a href="google.com.br" className="menu-item-link">Inicio</a></li>
                   <li className="menu-item"><a href="google.com.br" className="menu-item-link">Fale Conosco</a></li>
                   <li className="menu-item"><a href="google.com.br" className="menu-item-link">Conta</a></li>
-                  <li className="menu-item"><a href="google.com.br" className="menu-item-link">Carrinho</a></li>
+                  <li className="menu-item"><a href="google.com.br" className="menu-item-link">Carrinho ({cart.length})</a></li>
           </ul>
         </nav>
       </header>
+      <section>
+        {
+          data.map((product,index) => (
+            <div className='product-cotainer' key={index} >
+              <img src={product.img} className="product-img" alt="imagem do produto" width="100px" max-height="auto"/>
+              <h4 className="product-name" >{product.nome}</h4>
+              <h6 className="Product-price">{product.preco}</h6>
+              <button onClick={ () => handleCart(index) }>Adicionar ao carrinh</button>
+            </div>
+          ))
+        }
+      </section>
     </div>
   );
 }
---
+++ src/productData.js
@@ -0,0 +1,66 @@
+import img1 from './images/products/airpods.jpg';
+import img2 from './images/products/dell_inspiron_15.jpg';
+import img3 from './images/products/Echo_dot_3.jpg';
+import img4 from './images/products/galaxys21.jpg';
+import img5 from './images/products/iphone.jpg';
+import img6 from './images/products/jbl_go_2.jpg';
+import img7 from './images/products/kindle.jpg';
+import img8 from './images/products/mi_10_lite.jpg';
+import img9 from './images/products/playstation5.jpg';
+import img10 from './images/products/xbox.jpg';
+
+
+const productData = [
+    {
+        "nome":"Air Pods",
+        "img":img1,
+        "preco":1000
+    },
+    {
+        "nome":"Dell Inspiron i15",
+        "img":img2,
+        "preco":4000
+    },
+    {
+        "nome":"Echo Dot 3",
+        "img":img3,
+        "preco":"300"
+    },
+    {
+        "nome":"Galaxy s21",
+        "img":img4,
+        "preco":5000
+    },
+    {
+        "nome":"iPhone 12",
+        "img":img5,
+        "preco":8000
+    },
+    {
+        "nome":"JBL Go 2",
+        "img":img6,
+        "preco":200
+    },
+    {
+        "nome":"Kindle",
+        "img":img7,
+        "preco":200
+    },
+    {
+        "nome":"Xiaomi mi 11 lite",
+        "img":img8,
+        "preco":2000
+    },
+    {
+        "nome":"PlayStation 5",
+        "img":img9,
+        "preco":5000
+    },
+    {
+        "nome":"Xbox One Series S",
+        "img":img10,
+        "preco":2000
+    }
+]
+
+export default productData;
